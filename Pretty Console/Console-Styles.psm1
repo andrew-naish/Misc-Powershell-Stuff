0f28dd76479b4ce37da3fe9e54991b904026dd05
Andrew Naish <andrew.naish@wealth-dynamix.com>
2020-04-27 17:04:20 +0100

    singleline now incorporates message, also added ability ot modify ribbon char

--- Pretty Console/Console-Styles.psm1
+++ Pretty Console/Console-Styles.psm1
@@ -54,41 +54,57 @@ function Write-StepNotes {
     }
 }
 
-function New-ConsoleRibbon ([string]$Message, [switch]$SingleLine) {
+function New-ConsoleRibbon () {
+    param(
+        [Parameter(Mandatory=$true, Position=1)]
+        [String] $Message,
 
-    $lineChar = "="
-    $paddingChar = " "
-    $consoleWidth = ($Host.UI.RawUI.WindowSize.Width)-1
+        [Parameter(Mandatory=$false)]
+        [Char] $RibbonChar="=",
+
+        [Parameter(Mandatory=$false)]
+        [Switch] $SingleLine
+    )
+
+    $console_width = ($Host.UI.RawUI.WindowSize.Width)-1
 
     # construct full lines
     
-    [string]$fullLine = ""
-    for ($i=0; $i -lt $consoleWidth; $i++) {
-        $fullLine = $fullLine + $lineChar
+    [string]$full_line = ""
+    for ($i=0; $i -lt $console_width; $i++) {
+        $full_line = $full_line + $RibbonChar
     }
 
     # construct padding
     
-    $msgLength = $Message.Length
-    $paddingLength = (($consoleWidth-$msgLength)/2)-1
+    $message_length = $Message.Length
+    $padding_length = (($console_width-$message_length)/2)-1
     
     [string]$padding = ""
-    for ($i=0; $i -lt $paddingLength; $i++) {
-        $padding = $padding + $paddingChar
+    for ($i=0; $i -lt $padding_length; $i++) {
+        $padding = $padding + " "
     }
 
     # return 
     
-    if ($SingleLine) {
-        Write-Output $fullLine
-        return
+    if ($SingleLine)
+    {
+        $half_line  = ""
+        for ($i=0; $i -lt $padding_length-2; $i++) {
+            $half_line = $half_line + $RibbonChar
+        }
+        Write-Host "$(" {0} $Message {0}{1}{1}" -f $half_line, $RibbonChar)"
+    } 
+
+    else 
+    {
+        Write-Host $full_line
+        Write-Host ""
+        Write-Host ("{0}$Message" -f $padding)
+        Write-Host ""
+        Write-Host $full_line
     }
-    
-    Write-Output $fullLine
-    Write-Output ""
-    Write-Output ("{0}$Message" -f $padding)
-    Write-Output ""
-    Write-Output $fullLine
-    Write-Output ""
+
+    Write-Host ""
 
 }
